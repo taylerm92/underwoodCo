6008ddedd725716eb70fecd2aefece26a04dee7a
Austin Underwood <sbaustin951@gmail.com>
2016-11-22 14:21:50 -0500

    added the purchase file

---
+++ purchase.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <!-- Bootstrap MaxCDN -->
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="/underwoodCo/css/style.css">
+</head>
+<body>
+</body>
